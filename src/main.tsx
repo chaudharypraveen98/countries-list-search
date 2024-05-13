925d497ec6cce4ba664cdf8ff2c5491100ac76d3
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-05-13 23:21:39 +0530

    final

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ContextProvider } from './GlobalContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContextProvider>
   </React.StrictMode>,
 )
